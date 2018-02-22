1290283682336a41b13b661c67a094e100333419
Abrar Hussain <abrar.a.hussain@gmail.com>
2018-02-22 18:37:44 -0500

    Included React starter kit.

--- ouroboros/src/app.js
+++ ouroboros/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.disable('x-powered-by');
 
 app.use(logger('dev', {
-  skip: () => app.get('env') === 'test'
+  skip: () => app.get('env') === 'test',
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,4 +29,5 @@ app.use((req, res) => {
   res.type('txt').send('Not found');
 });
 
-export default app;
+const { PORT = 8080 } = process.env;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
