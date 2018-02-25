80c049ded4045756a5c8d60f361ad891afb7011f
Abrar Hussain <abrar.a.hussain@gmail.com>
2018-02-25 16:05:17 -0500

    Made small change to separate components and connected components.

---
+++ scythe/src/containers/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { push } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync
+} from '../reducers/counter';
+import Home from '../components/index.js';
+
+
+const mapStateToProps = state => ({
+  count: state.counter.count,
+  isIncrementing: state.counter.isIncrementing,
+  isDecrementing: state.counter.isDecrementing
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync,
+  changePage: () => push('/about-us')
+}, dispatch)
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
