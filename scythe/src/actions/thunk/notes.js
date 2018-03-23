2838d5e07d7cf6d4da69c13da59f27311c9a40b7
David Ansermino <david@chainsafe.io>
2018-03-22 20:47:19 -0400

    Fixed leftover 'gaping hole'

--- scythe/src/actions/thunk/notes.js
+++ scythe/src/actions/thunk/notes.js
@@ -4,60 +4,54 @@ import { ouroborosEndpoint } from '../../constants/services';
 import { getResolvedNote } from '../../reducers/notes';
 
 
-export const addNoteThunk = text => async (dispatch) => {
-  try {
-    console.log(ouroborosEndpoint);
-    dispatch(disableNotes());
-    return fetch(`${ouroborosEndpoint}/tickets/updateTicket`, { method: 'POST', body: text })
-      .then((response) => {
-        if (response.status === 200) {
-          dispatch(confirmAddNote(text));
-        } else {
-          console.log(response.statusText);
-        }
-        dispatch(enableNotes());
-      });
-  } catch (error) {
-    console.log(error);
-    dispatch(enableNotes());
-  }
+export const addNoteThunk = text => (dispatch) => {
+  console.log(ouroborosEndpoint);
+  dispatch(disableNotes());
+  return fetch(`${ouroborosEndpoint}/tickets/updateTicket`, { method: 'POST', body: text })
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(confirmAddNote(text));
+      } else {
+        console.log(response.statusText);
+      }
+      dispatch(enableNotes());
+    }).catch((err) => {
+      console.log(err);
+      dispatch(enableNotes());
+    });
 };
 
-export const deleteNoteThunk = (index, note, ticket) => async (dispatch) => {
-  try {
-    dispatch(disableNotes());
-    return fetch(`${ouroborosEndpoint}/tickets/updateTicket`, { method: 'POST', body: deleteNote(index, ticket) })
-      .then((response) => {
-        if (response.status === 200) {
-          dispatch(confirmDeleteNote(index));
-        } else {
-          console.log(response.statusText);
-        }
-        dispatch(enableNotes());
-      });
-  } catch (error) {
-    console.log(error);
-    dispatch(enableNotes());
-  }
+export const deleteNoteThunk = (index, note, ticket) => (dispatch) => {
+  dispatch(disableNotes());
+  return fetch(`${ouroborosEndpoint}/tickets/updateTicket`, { method: 'POST', body: deleteNote(index, ticket) })
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(confirmDeleteNote(index));
+      } else {
+        console.log(response.statusText);
+      }
+      dispatch(enableNotes());
+    }).catch((err) => {
+      console.log(err);
+      dispatch(enableNotes());
+    });
 };
 
-export const resolveNoteThunk = (index, note, ticket) => async (dispatch) => {
-  try {
-    console.log(ouroborosEndpoint);
-    dispatch(disableNotes());
-    return fetch(`${ouroborosEndpoint}/tickets/updateTicket`, { method: 'POST', body: resolveNote(index, note, ticket) })
-      .then((response) => {
-        if (response.status === 200) {
-          dispatch(confirmResolveNote(index, note));
-        } else {
-          console.log(response.statusText);
-        }
-        dispatch(enableNotes());
-      });
-  } catch (error) {
-    console.log(error);
-    dispatch(enableNotes());
-  }
+export const resolveNoteThunk = (index, note, ticket) => (dispatch) => {
+  console.log(ouroborosEndpoint);
+  dispatch(disableNotes());
+  return fetch(`${ouroborosEndpoint}/tickets/updateTicket`, { method: 'POST', body: resolveNote(index, note, ticket) })
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(confirmResolveNote(index, note));
+      } else {
+        console.log(response.statusText);
+      }
+      dispatch(enableNotes());
+    }).catch((err) => {
+      console.log(err);
+      dispatch(enableNotes());
+    });
 };
 
 // Returns a new ticket obj with the note marked as resolved
